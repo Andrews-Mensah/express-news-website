260317d593b173815c3a4885bb02db048be2c873
Andrews Mensah <andrews.mensah@amalitech.org>
2021-09-25 21:13:19 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express ();
+
+const port = 3000; 
+
+
+app.use(express.static('public'))
+app.use('/css', express.static (__dirname + 'public/css'))
+app.use('/img', express.static (__dirname + 'public/img'))
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+
+
+const newsRouter = require('./routes/home')
+
+app.use('/', newsRouter)
+app.use('/article', newsRouter)
+
+
+
+
+
+app.listen(port, ()=> {
+    console.log(`Server listening on port ${port}`)
+})
---
+++ routes/home.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const newsRouter = express.Router();
+const axios = require('axios');
+
+
+newsRouter.get('', async(req, res,next)=>{
+    // res.render('home')
+
+    try{
+        
+        const newsAPI = await axios.get(`https://ghgossip.com/wp-json/wp/v2/posts`)
+        res.render('home', {articles: newsAPI.data})
+
+
+    } catch (err){
+        if (err.response){
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if (err.request){
+            console.log(err.request)
+
+        } else {
+            console.error('Error', err.message)
+
+        }
+
+    }
+
+})
+
+
+
+
+module.exports = newsRouter
